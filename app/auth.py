2ad96db4ef047760f01479c9be45a11b1d996aee
Dinyar <migranovdiniar@gmail.com>
2025-11-18 23:23:27 +0300

    created a simple authentication

--- app/auth.py
+++ app/auth.py
@@ -1,1 +1,23 @@
-from 
+from app.db import get_connection
+
+DEFAULT_PASSWORDS = {
+    "admin": "admin123",
+    "student": "student123",
+    "teacher": "teacher123"
+}
+
+def authenticate(email: str, password: str):
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT user_id, name, email, role FROM users WHERE email = ?", (email,))
+    row = cur.fetchone()
+    conn.close()
+
+    if not row:
+        return None
+    role = row['role'] # Проверяю, что там с ролью - базочка
+
+    expected = DEFAULT_PASSWORDS.get(role)
+    if expected and password == expected:
+        return dict(role)
+    return None
